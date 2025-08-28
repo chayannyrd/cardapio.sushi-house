da28e8a8461155d355c80bf9bf9ba77f89ccbc82
chayanny <chayannytj@gmail.com>
2025-08-28 16:38:54 -0300

    add worflow

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
+/** @type {import('next').NextConfig} */
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
   poweredByHeader: false,
   compiler: { styledComponents: { ssr: true } },
+  images: {
+    unoptimized: true, // se você estiver usando <Image /> do Next
+  },
+
 };
 
 export default nextConfig;
+
