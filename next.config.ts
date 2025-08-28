972d4a1e08284c62ac296a4594d2c0e23d2fafa8
chayanny <chayannytj@gmail.com>
2025-08-28 17:48:35 -0300

    next-config 3

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,10 @@ const nextConfig: NextConfig = {
   output: 'export',
   poweredByHeader: false,
   compiler: { styledComponents: { ssr: true } },
+  images: {
+    unoptimized: true,
+  },
+  basePath: 'cardapio.sushi-house'
 
 };
 
